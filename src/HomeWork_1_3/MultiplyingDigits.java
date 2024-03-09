ea1137d095f4f382cea28ff48f1cce9d9369c482
ArcBLR <arc11041977@gmail.com>
2024-03-09 23:59:44 +0300

    Добавлено описание в packege HomeWork_1_3

--- src/HomeWork_1_3/MultiplyingDigits.java
+++ src/HomeWork_1_3/MultiplyingDigits.java
@@ -8,8 +8,10 @@ public class MultiplyingDigits {
         System.out.print("Введите число: ");
         int num = in.nextInt();
         int num1 = num;
-//        int numDigits = String.valueOf(num).length();
-        int numDigits = (int)Math.log10(num) + 1;  // меньше потребление ресурсов
+//        int numDigits = String.valueOf(num).length(); //вводимое число преобразуется в строку и считаются символы
+        int numDigits = (int) Math.log10(num) + 1;  /* берется Log по основанию 10, он возвращает колличество цифр
+                                                       в числе -1 и по этому добавляем +1
+                                                       меньше потребление ресурсов*/
         int m = 1;
         for (int i = 0; i < numDigits; i++) {
             m *= num % 10;
