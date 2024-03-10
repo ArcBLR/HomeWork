0672bacc126e47fc0be3a8ff795361b2f5a1ae5a
ArcBLR <arc11041977@gmail.com>
2024-03-10 09:16:43 +0300

    Изменение метода наименование пакетов

--- src/HomeWork_1_1_3/MultiplyingDigits.java
+++ src/HomeWork_1_1_3/MultiplyingDigits.java
@@ -8,10 +8,11 @@ public class MultiplyingDigits {
         System.out.print("Введите число: ");
         int num = in.nextInt();
         int num1 = num;
-//        int numDigits = String.valueOf(num).length(); //вводимое число преобразуется в строку и считаются символы
+//        int numDigits = Integer.toString(num).length(); //вводимое число преобразуется в строку и считаются символы
+//        int numDigits = String.valueOf(num).length();   //аналог Integer.toString
         int numDigits = (int) Math.log10(num) + 1;  /* берется Log по основанию 10, он возвращает колличество цифр
-                                                       в числе -1 и по этому добавляем +1
-                                                       меньше потребление ресурсов*/
+//                                                       в числе -1 и по этому добавляем +1
+//                                                       меньше потребление ресурсов*/
         int m = 1;
         for (int i = 0; i < numDigits; i++) {
             m *= num % 10;
