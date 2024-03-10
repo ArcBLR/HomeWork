da1b55bf5b4e1f333122aa3a1af676463a8f1021
ArcBLR <arc11041977@gmail.com>
2024-03-10 19:00:08 +0300

    добавлено решение ещё одного задания

--- src/HomeWork_1_2_1/Massif.java
+++ src/HomeWork_1_2_1/Massif.java
@@ -4,13 +4,13 @@ public class Massif {
     public static void main(String[] args) {
         int[] array = new int[]{1, 2, 3, 2, 5, 6, 0, 8, 9};
         System.out.println("Перебор массива циклом for");
-        for (int i = 0; i < array.length; i++){
-        System.out.print(array[i]);
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
 
         }
         System.out.println("");
         System.out.println("Перебор массива циклом foreach");
-        for (int i:array){
+        for (int i : array) {
             System.out.print(i);
         }
         System.out.println("");
@@ -21,8 +21,8 @@ public class Massif {
                         {4, 5, 6},
                         {7, 8, 9}
                 };
-        for (int i = 0; i < array2.length; i++){
-            for(int j=0; j < array2[i].length; j++){
+        for (int i = 0; i < array2.length; i++) {
+            for (int j = 0; j < array2[i].length; j++) {
 
                 System.out.printf("%d ", array2[i][j]);
             }
@@ -30,7 +30,7 @@ public class Massif {
         }
         System.out.println("");
         System.out.println("Перебор массива циклом do...while");
-        int k=0;
+        int k = 0;
         do {
             System.out.print(array[k]);
             k++;
@@ -38,8 +38,8 @@ public class Massif {
         while (k < array.length);
         System.out.println("");
         System.out.println("Перебор массива циклом while");
-        int j= 0;
-        while (j< array.length){
+        int j = 0;
+        while (j < array.length) {
             System.out.print(array[j]);
             j++;
 
--- src/HomeWork_1_2_2/ArrayElement.java
+++ src/HomeWork_1_2_2/ArrayElement.java
@@ -3,9 +3,9 @@ package HomeWork_1_2_2;
 public class ArrayElement {
     public static void main(String[] args) {
         System.out.println("Выводим каждый второй элемент массива");
-        int [] array = new int [] {1,2,3,4,5,6,7,8,9,11,12,13,14,15,16,17,18,19,21};
-        for (int i = 1; i< array.length; i+=2){
-            System.out.print(array[i]+" ");
+        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21};
+        for (int i = 1; i < array.length; i += 2) {
+            System.out.print(array[i] + " ");
 
         }
     }
