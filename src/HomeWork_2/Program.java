3df7657a47d4bf4959a38bb30b21e747787101a0
ArcBLR <arc11041977@gmail.com>
2024-03-14 16:15:26 +0300

    Немного исправил последнюю задачу.

--- src/HomeWork_2/Program.java
+++ src/HomeWork_2/Program.java
@@ -2,7 +2,7 @@ package HomeWork_2;
 
 public class Program {
     public static void main(String[] args) {
-        Car car=new Car(new Tire(Season.SUMMER,"R-19","Michlen"),new Engine(1800,"B4184S2","АИ-95"));
+        Car car=new Car(new Tire(Season.SUMMER,16,"Michelin"),new Engine(1800,"B4184S2","АИ-95"));
         System.out.println(car);
     }
 }
