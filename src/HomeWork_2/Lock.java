645c4d36b6bef6d4f8291406bc34ece081d81ccf
ArcBLR <arc11041977@gmail.com>
2024-03-15 21:57:33 +0300

    Немного исправил последнюю задачу.

---
+++ src/HomeWork_2/Lock.java
@@ -0,0 +1,14 @@
+package HomeWork_2;
+
+public class Lock {
+    private int key;
+
+    public Lock(int key) {
+        this.key = key;
+    }
+
+    @Override
+    public String toString() {
+        return "";
+    }
+}
