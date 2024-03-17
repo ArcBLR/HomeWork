0a17e40a501d49dbf0a6100eaf9a593a3fa33066
ArcBLR <arc11041977@gmail.com>
2024-03-17 06:37:49 +0300

    создал класс Drivers

--- src/HomeWork_2/Driver.java
+++
@@ -1,7 +0,0 @@
-package HomeWork_2;
-
-public class Driver {
-    private String drivingExperiens;
-    private String drivingCategoryRights;
-    private String availabilityKeys;
-}
---
+++ src/HomeWork_2/Drivers.java
@@ -0,0 +1,9 @@
+package HomeWork_2;
+
+public class Drivers {
+    private String drivingCategoryRights;
+    private String drivingExperiens;
+
+
+}
+
